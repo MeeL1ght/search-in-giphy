06abaf459e98bdd2ce2b2b6018e60e2bb4b4334a
Moises Reyes <meelight12@gmail.com>
2023-03-03 22:48:05 -0400

    update |  hook: useFetchGifs :v

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { getGifs } from './../helpers/getGifs.js'
 
-export const useFetchGifs = (category) => {
+export const useFetchGifs = category => {
   const [state, setState] = useState({
     data: [],
     loading: true
