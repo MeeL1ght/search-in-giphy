bcaddbd3542b7a37129a2123aa222d68ff351fa4
Moises Reyes <meelight12@gmail.com>
2023-03-03 22:38:16 -0400

    update vite.config :v

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig(({ command, mode }) => {
-  const env        = loadEnv(mode, process.cwd())
+  const env      = loadEnv(mode, process.cwd())
   const devSetup = {
     plugins: [react()],
     server: {
